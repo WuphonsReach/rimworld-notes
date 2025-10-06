c73fb1ca8bd4ce2ec2f655dcb97e95047a2cf3d4
WuphonsReach <wuphonsreach@wuphonsreach.org>
2025-10-06 11:35:10 -0400

    snapshot the logs folder too

--- snapshot-mods-and-settings.sh
+++ snapshot-mods-and-settings.sh
@@ -11,7 +11,7 @@ pushd rimworld-logs
 ./grab-latest.sh
 popd
 
-git add "rimworld-config/" "rimworld-mod-lists/"
+git add "rimworld-config/" "rimworld-mod-lists/" "rimworld-logs/"
 
 #current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
 #echo "date=$current_datetime"
